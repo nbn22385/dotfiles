ae01ad8b3371071e6856ff7d30bb70d39d1b2314
nbn22385 <nbn22385@gmail.com>
2020-09-05 13:03:59 -0400

    minpac silent install

--- install.sh
+++ install.sh
@@ -2,10 +2,10 @@
  
 # Install dotfiles as symbolic links using 'stow'
 if ! type stow > /dev/null; then echo "ERROR: gnu 'stow' package not installed" && exit 1; fi
-stow vim tmux zsh --target "$HOME"
- 
+stow --no-folding vim tmux zsh --target "$HOME"
+
 # Vim plugin management using minpac
-git clone --quiet "https://github.com/k-takata/minpac" "$HOME/.vim/pack/minpac/opt"
-vim -E -s +"call minpac#update()" +qall
- 
-echo "Done setting up dotfiles"
+git clone --quiet https://github.com/k-takata/minpac.git ~/.vim/pack/minpac/opt/minpac
+vim --noplugin +PackUpdateAndQuit
+
+echo "Done setting up dotfiles."
