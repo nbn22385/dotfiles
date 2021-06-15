2c12706b5e1b8a2a063d6e58ec38686a39ecf0a8
nbn22385 <nbn22385@gmail.com>
2021-06-14 20:30:21 -0400

    install config folder

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ if [ "$1" = "--install-dependencies" ]; then ./tools/dependencies.sh; fi
 # Install dotfiles as symbolic links using 'stow'
 if ! type stow > /dev/null; then echo "ERROR: gnu 'stow' package not installed" && exit 1; fi
 stow --no-folding --target="$HOME" \
-  vim tmux zsh 
+  config vim tmux zsh 
 
 # Vim plugin management using minpac
 git clone --quiet https://github.com/k-takata/minpac.git ~/.vim/pack/minpac/opt/minpac
