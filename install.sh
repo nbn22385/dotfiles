9cc34aad4a3c49396fc0996ab8bb1a06479765b8
nbn22385 <nbn22385@gmail.com>
2021-07-15 11:24:37 -0400

    add restow flag to install script

--- install.sh
+++ install.sh
@@ -7,8 +7,8 @@ if [ "$1" = "--install-dependencies" ]; then ./tools/dependencies.sh; fi
 # Check that stow is installed on the system
 if ! type stow > /dev/null; then echo "ERROR: gnu 'stow' package not installed" && exit 1; fi
 
-# Deploy the symlinks
-stow --target="$HOME" $STOW_FOLDERS
+# First unstow then stow the symlinks
+stow --restow --target="$HOME" $STOW_FOLDERS
 
 # Install vim plugins
 git clone --quiet https://github.com/k-takata/minpac.git ~/.vim/pack/minpac/opt/minpac > /dev/null 2>&1
