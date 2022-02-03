03f0d6b81cfc053920ed6cfd4b0587668b9f2c36
nbn22385 <nbn22385@gmail.com>
2022-02-03 17:25:08 -0500

    switch to fnune repo for base16-shell

--- extras/dependencies.sh
+++ extras/dependencies.sh
@@ -85,7 +85,7 @@ else
 fi
 
 echo "[+] Installing base16-shell"
-git clone https://github.com/chriskempson/base16-shell.git ~/.config/base16-shell > /dev/null 2>&1
+git clone https://github.com/fnune/base16-shell.git ~/.config/base16-shell > /dev/null 2>&1
 
 echo "[+] Installing powerlevel10k prompt"
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k > /dev/null 2>&1
