1695d2a5d539967493394449714141936d111ede
nbn22385 <nbn22385@gmail.com>
2023-09-03 12:45:14 -0400

    set max length for media item

--- config/.config/sketchybar/plugins/media.sh
+++ config/.config/sketchybar/plugins/media.sh
@@ -3,12 +3,13 @@
 source "$CONFIG_DIR/colors.sh"
 
 STATE="$(echo "$INFO" | jq -r '.state')"
+MAXLEN=64
 
 if [ "$STATE" = "playing" ]; then
-  MEDIA="$(echo "$INFO" | jq -r '"󰎈  " + .artist + " - " + .title')"
+  MEDIA="$(echo "$INFO" | jq -r '"󰎈  " + .artist + " - " + .title'[:$MAXLEN])"
   sketchybar --set $NAME label="$MEDIA" drawing=on label.color=$WHITE
 else
   # sketchybar --set $NAME drawing=off
-  MEDIA="$(echo "$INFO" | jq -r '"󰎊  " + .artist + " - " + .title')"
+  MEDIA="$(echo "$INFO" | jq -r '"󰎊  " + .artist + " - " + .title'[:$MAXLEN])"
   sketchybar --set $NAME label="$MEDIA" drawing=on label.color=$GREY
 fi
